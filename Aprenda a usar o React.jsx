2130d6c10aefe5e30e1d529216ee1915cfc1dfda
Kimi Djan Paes Teixeira <71793652+fbicomunista@users.noreply.github.com>
2022-08-01 11:37:03 -0300

    Update Aprenda a usar o React.jsx

--- Aprenda a usar o React.jsx
+++ Aprenda a usar o React.jsx
@@ -25,7 +25,7 @@
    <Segundo></Segundo> ## componente com o nome de Segundo
    <Terceiro></Terceiro> ## componentes com o nome de Terceiro
 
- # porem, não podemos colocar Componetes de maneira proxima, para isso usamos uma "Tag" especifica que ira entre os componentes
+ # porem, não podemos colocar Componetes de maneira proxima, para isso usamos uma "Tag" especifica que ira entre os componentes, permitindo que eles se mostrem          proximos um do outro
  
  
  
@@ -33,119 +33,6 @@
  @ {/*  */} # assim que se escreve um comentario em React
 
  # {/* Ola! eu sou um comentario */} 
-
-
- 
-
-
- @@ ReactDOM.render{
- 	<Primeiro></Primeiro> ## criei um componente chamado "Primeiro"
- 
- 
- }
-	
-	 
-		<Primeiro></Primeiro> ## todo componente em React 
-		<ComParametro titulo="Esse é o titulo" subtitulo="Esse é um subtitulo"></comParametro>
-	 ## 
-	document.getElementById("root")
-
-
-
- export default parametros =>
-	<>
-		<h3>{parametros.titulo}</h3>
-		<p>{array(10).fill(0)}</p> # ira preexer uma array toda pelo numero 0
-	</> # tudo que estiver entre {} o React ira interpretar como codigo JavaScript
-
-
- ReactDOM.render(
-	<div className=""> # é obrigatorio no React usar "className" ao inves só de "Class"
-	</div>
-	);  
-
-
-
- type TweetProps = {
-	text: string; # pode ser colocado um ponto de interrogação depois de text para descrever aquela tipagem é opcional por exemplo: text?: string;
- } # tipa a variavel "TweetProps" como uma string
-
-
-
- type dropBox = {
-	text:string;
-}
-
-
-
- export function Tweet(props: dropBox){
-	return (
-	<p>{props.text}</p>
- )
- }
-
-
-
- export function Tweet(props: TweetProps) {
-	return (
-		<p>Tweet</p>
-	); # repassa a tipagem do "TweetPromps" para o "Tweet"
-}
-
-
-
-
-
-
-
-
-
-
- function App(){
-	const (tweet, setTweets) = useState =><string[]>({
-		"Tweet 1",
-		"Tweet 2",
-		"Tweet 3"
-
-	}) # useState é uma função do React que toda vez que uma variavel/constante muda o seu valor todo o componente sera renderizado novamente e utilizando as <>  (dinerigs) é preciso colocar uma lista de string logo, <string[]>
-
-
-
- function createTweet(){
-	setTweets([Tweets, "Tweet 5"])
-
-}
-
-	return (
-		<div>
-		{Tweets.map(tweet => {
-			return <Tweet text={tweet} />
-		} # criei um laço de repetição que para cada tweet que tiver na lista 
-
-		<button onclick= {createTweet} style{{ }}> Adicionar tweet </button>
-		</div>
-
-
-
-	<div>
-
-		
-		{Tweet.map(Tweet => {
-			return <Tweet text={tweet}/>
-
-
-
-
-
-	return (
-		<div>
-			<Tweet text="Tweet 1"/>
-			<Tweet text= )
-
-}
-
-
-
 
 
 
